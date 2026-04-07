8c823e2d23ac55beb8ce39d13d4d90d2b0aadea2
Robert MacCracken <robert.maccracken@gmail.com>
2026-04-07 13:50:02 -0700

    fixing repo

--- fuzz/fuzz_targets/fuzz_bandwidth.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        ai_hwaccel::fuzz_helpers::fuzz_nvidia_bandwidth_parser(s);
-        ai_hwaccel::fuzz_helpers::fuzz_dpm_clock_parser(s);
-    }
-});
--- fuzz/fuzz_targets/fuzz_new_backends.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        ai_hwaccel::fuzz_helpers::fuzz_cerebras_parser(s);
-        ai_hwaccel::fuzz_helpers::fuzz_graphcore_parser(s);
-    }
-});
